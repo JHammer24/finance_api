1a699c44b394d965a2f9d788ebe99c676a1cb18b
Kazantsev Artyom <kazantsev.av@phystech.edu>
2025-04-30 23:52:23 +0300

    fix(auth): исправить проблему с аутентификацией

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(auth_router)
+app.include_router(auth_router, prefix="/auth")
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(categories.router, prefix="/categories", tags=["categories"])
 app.include_router(transactions.router, prefix="/transactions", tags=["transactions"])
